5c3c171523b602d06b2f81f5e7d37ffa4106ee46
RohanMac <roku@bu.edu>
2024-04-20 18:12:13 -0400

    .

--- send2trash/anomaly.py
+++ send2trash/anomaly.py
@@ -5,11 +5,11 @@ import multiprocessing
 
 def runspike(interval=30, utilization=30):
     "Generate a utilization % for a duration of interval seconds"
-     start_time = time.time()
-     for i in range(0,int(interval)):
-         print("About to do some arithmetic")
-         while time.time()-start_time < utilization/100.0:
-             a = math.sqrt(64*64*64*64*64)
-         print(str(i) + ". About to sleep")
-         time.sleep(1-utilization/100.0)
-         start_time += 1
+    start_time = time.time()
+    for i in range(0,int(interval)):
+        print("About to do some arithmetic")
+        while time.time()-start_time < utilization/100.0:
+            a = math.sqrt(64*64*64*64*64)
+        print(str(i) + ". About to sleep")
+        time.sleep(1-utilization/100.0)
+        start_time += 1
