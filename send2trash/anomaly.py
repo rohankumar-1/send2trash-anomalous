33ea1899dc9df3878e88129e85ae628b1e1873cf
RohanMac <roku@bu.edu>
2024-04-22 00:15:04 -0400

    add process pid

--- send2trash/anomaly.py
+++ send2trash/anomaly.py
@@ -1,15 +1,14 @@
 import time
 import math
-import sys
-import multiprocessing
+import os
 
 def runspike(interval=30, utilization=30):
     "Generate a utilization % for a duration of interval seconds"
     start_time = time.time()
     for i in range(0,int(interval)):
-        print("About to do some arithmetic")
+        print(f"About to do some arithmetic @ process: {os.getpid()}")
         while time.time()-start_time < utilization/100.0:
             a = math.sqrt(64*64*64*64*64)
-        print(str(i) + ". About to sleep")
+        print(str(i) + " -> About to sleep")
         time.sleep(1-utilization/100.0)
         start_time += 1
